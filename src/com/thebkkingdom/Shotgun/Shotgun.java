ed715d76afd830aee61c6f8bce0b845bd518018b
Taylor Cornett <cornetter@gmail.com>
2011-12-20 15:04:35 -0800

    NPE fixes

--- src/com/thebkkingdom/Shotgun/Shotgun.java
+++ src/com/thebkkingdom/Shotgun/Shotgun.java
@@ -2,9 +2,14 @@ package com.thebkkingdom.Shotgun;
 
 import java.util.logging.Level;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event.Type;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.thebkkingdom.Shotgun.Listeners.EventsHandler;
+import com.thebkkingdom.Shotgun.Listeners.entityListener;
+import com.thebkkingdom.Shotgun.Listeners.playerListener;
 
 public class Shotgun extends JavaPlugin {
 
@@ -18,15 +23,23 @@ public class Shotgun extends JavaPlugin {
 
 	public void onEnable() {
 		registerCommands();
-		EventsHandler.registerEvents();
+		registerEvents();
 		ShotgunConfiguration.setup();
 
 		ShotgunLogger.log(Level.INFO, " Enabled version: "
 				+ getDescription().getVersion());
 	}
-	
-	private void registerCommands(){
+
+	private void registerCommands() {
 		getCommand("s").setExecutor(new ShotgunCommands());
+	}
+
+	private void registerEvents() {
+		PluginManager pm = Bukkit.getServer().getPluginManager();
+		pm.registerEvent(Type.PLAYER_INTERACT, new playerListener(),
+				Priority.Normal, this);
+		pm.registerEvent(Type.PROJECTILE_HIT, new entityListener(),
+				Priority.Normal, this);
 	}
 
 	public static Shotgun getInstance() {
